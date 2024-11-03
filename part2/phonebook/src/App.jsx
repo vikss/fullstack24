764c07cc0baf166642fc1811edd3081de311c205
viraktijain <viraktijain@commvault.com>
2024-11-03 23:10:17 +0530

    Added an effect hook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Persons from './Persons'
 import PersonForm from './PersonForm'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(()=>{
+    axios.get("http://localhost:3001/persons").then( (response)=>{
+
+console.log(response.data)
+setPersons(response.data)
+
+    })
+
+
+
+
+  }, [])
  
   const handleNameAdd = (event)=>{
  console.log(event.target.value)
