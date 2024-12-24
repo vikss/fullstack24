b4baab98fb76470130c0626c825aabb4593c4f08
Virakti Jain <viraktijain@Viraktis-MacBook-Pro.local>
2024-12-24 23:52:18 +0530

    Final changes

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,7 +4,6 @@ import PersonForm from './PersonForm'
 import personService from './services/persons'
 import './index.css'
 
-
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
@@ -14,14 +13,10 @@ const App = () => {
   const [error, setError] = useState(false)
 
   useEffect(() => {
-    personService.getAll().then(data => {
+    personService.getAll().then((data) => {
       console.log('Getting all entries ', data)
       setPersons(data)
     })
-
-
-
-
   }, [])
 
   const handleNameAdd = (event) => {
@@ -41,82 +36,94 @@ const App = () => {
     setFilter(filterTerm)
   }
 
-
   const handleForm = (event) => {
     event.preventDefault()
-    console.log("Adding a new name in the directory")
+    console.log('Adding a new name in the directory')
 
-    let idArr = persons.map(p => p.id)
-    console.log("Ids of persons present", idArr)
+    let idArr = persons.map((p) => p.id)
+    console.log('Ids of persons present', idArr)
     let id = Math.max(...idArr) + 1
     id = id.toString()
     const personObj = { name: newName, number: newNumber, id }
-    let names = persons.map(person => person.name.toLowerCase())
+    let names = persons.map((person) => person.name.toLowerCase())
     let namePresent = names.includes(newName.toLowerCase())
     console.log(`Is the name present? ${namePresent}`)
     if (!namePresent) {
-      console.log("Creating a new phonebook entry", personObj)
-      personService.create(personObj).then(res => {
-        console.log("Response from post is ", res)
-        const jsObject = JSON.parse(res)
-        setPersons(persons.concat(jsObject))
-        console.log("New persons array is ", persons)
-        setMessage(`Added ${jsObject.name}`)
-        setError(false)
-
-
-        setTimeout(() => {
-          setMessage(null)
+      console.log('Creating a new phonebook entry for this person ', personObj)
+      personService
+        .create(personObj)
+        .then((res) => {
+          console.log('Response from post is ', res)
+          const jsObject = JSON.parse(res)
+          setPersons(persons.concat(jsObject))
+          console.log('New persons array is ', persons)
+          setMessage(`Added ${jsObject.name}`)
           setError(false)
-        }, 10000)
 
+          setTimeout(() => {
+            setMessage(null)
+            setError(false)
+          }, 10000)
+        })
+        .catch((err) => {
+          console.log('Error occurred while adding an entry', err)
+          setMessage(err.response.data.error)
 
-      })
-
-    }
-    else {
-      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
-        let oldPersonEntry = persons.find(person => person.name.toLowerCase() === newName.toLowerCase())
-        console.log("Id of the already existing entry ", oldPersonEntry.id)
-        personService.updateNumber(oldPersonEntry.id, personObj).then(res => {
-          let newPersonsArr = persons.filter(person => person.id != oldPersonEntry.id)
-
+          setError(true)
+          setTimeout(() => {
+            setMessage(null)
+            setError(false)
+          }, 10000)
+        })
+    } else {
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        let oldPersonEntry = persons.find(
+          (person) => person.name.toLowerCase() === newName.toLowerCase()
+        )
+        console.log('Id of the already existing entry ', oldPersonEntry.id)
+        personService.updateNumber(oldPersonEntry.id, personObj).then((res) => {
+          let newPersonsArr = persons.filter(
+            (person) => person.id != oldPersonEntry.id
+          )
 
           console.log(res)
           setPersons(newPersonsArr.concat(res))
-
-
         })
-
       }
     }
     setNewName('')
     setNewNumber('')
-
   }
   const handleDelete = (event) => {
     console.log(event.target.value)
     const idToDelete = event.target.value
-    const personName = persons.find(p => p.id === idToDelete).name
+    const personName = persons.find((p) => p.id === idToDelete).name
     if (window.confirm(`Delete ${personName}?`)) {
-
-      personService.deleteEntry(idToDelete).then(res => {
-        
-        let newPersonsArray = persons.filter(p => p.id != idToDelete)
-        console.log('Persons array after delete operation is ', newPersonsArray)
-        setPersons(newPersonsArray)
-      }).catch(err => {
-        console.log(`Error occurred while deleting an entry ${err}`)
-        setMessage(`Information of ${personName} has already been removed from the server`)
-        setError(true)
-        setTimeout(() => {
-
-          setMessage(null)
-          setError(false)
-        }, 10000)
-
-      }
-      )
+      personService
+        .deleteEntry(idToDelete)
+        .then((res) => {
+          let newPersonsArray = persons.filter((p) => p.id != idToDelete)
+          console.log(
+            'Persons array after delete operation is ',
+            newPersonsArray
+          )
+          setPersons(newPersonsArray)
+        })
+        .catch((err) => {
+          console.log(`Error occurred while deleting an entry ${err}`)
+          setMessage(
+            `Information of ${personName} has already been removed from the server`
+          )
+          setError(true)
+          setTimeout(() => {
+            setMessage(null)
+            setError(false)
+          }, 10000)
+        })
     }
   }
   return (
@@ -124,24 +131,36 @@ const App = () => {
       <h2>Phonebook</h2>
       <Notification message={message} error={error}></Notification>
       <Filter filter={filter} handleFilterChange={handleFilterChange}></Filter>
-      <PersonForm handleForm={handleForm} name={newName} number={newNumber} handleNameAdd={handleNameAdd} handleNumberAdd={handleNumberAdd}></PersonForm>
+      <PersonForm
+        handleForm={handleForm}
+        name={newName}
+        number={newNumber}
+        handleNameAdd={handleNameAdd}
+        handleNumberAdd={handleNumberAdd}
+      ></PersonForm>
       <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter} handleDelete={handleDelete} ></Persons>
+      <Persons
+        persons={persons}
+        filter={filter}
+        handleDelete={handleDelete}
+      ></Persons>
     </div>
   )
 }
 const Notification = ({ message, error }) => {
+  console.log('Setting a notification')
   console.log(message, error)
-  if (error)
-    return <div className='error'>{message}</div>
-  else if (message)
-    return <div className="message">{message}</div>
+  if (error) return <div className='error'>{message}</div>
+  else if (message) return <div className='message'>{message}</div>
   return <div></div>
-
 }
 const Filter = (props) => {
-  return <div>filter shown with <input value={props.filter} onChange={props.handleFilterChange}></input></div>
+  return (
+    <div>
+      filter shown with{' '}
+      <input value={props.filter} onChange={props.handleFilterChange}></input>
+    </div>
+  )
 }
 
 export default App
-
